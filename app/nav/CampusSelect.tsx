6048d4797ed7e0c29150cbc492211bf32172134a
asdacosta <asdacosta18@gmail.com>
2025-01-26 23:19:15 +0000

    Set up input to list all campuses

---
+++ app/nav/CampusSelect.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { useState } from "react";
+
+function CampusSelect() {
+  const [chosenCampus, setChosenCampus] = useState("");
+
+  const handleCampusChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setChosenCampus(event.target.value);
+  };
+
+  return (
+    <div>
+      <select
+        name="campus-name"
+        value={chosenCampus}
+        onChange={handleCampusChange}
+      >
+        <option value="" disabled>
+          Campus
+        </option>
+        <option value="legon">Legon</option>
+        <option value="knust">KNUST</option>
+      </select>
+    </div>
+  );
+}
+
+export { CampusSelect };
