499f74db98da234b80a1a97acf612f4419b1b147
asdacosta <asdacosta18@gmail.com>
2025-01-27 00:21:51 +0000

    Add classes

--- app/nav/page.tsx
+++ app/nav/page.tsx
@@ -1,29 +1,31 @@
 import Link from "next/link";
 import React from "react";
-import { SearchInput } from "./SearchInput";
-import { CampusSelect } from "./CampusSelect";
+import { SearchInput } from "./SearchInput/SearchInput";
+import { CampusSelect } from "./CampusSelect/CampusSelect";
+import styles from "./page.module.css";
 
 function page() {
   return (
-    <nav>
-      <section>
-        <Link href="/">
-          <h1>CampusairX</h1>
-        </Link>
+    <nav className={styles.nav}>
+      <section className={styles.primary}>
+        <section className={styles.logoBox}>
+          <Link href="/">
+            <h1>CampusairX</h1>
+          </Link>
+        </section>
+        <section className={styles.inputsBox}>
+          <SearchInput />
+          <CampusSelect />
+        </section>
       </section>
 
-      <section>
-        <SearchInput />
-        <CampusSelect />
-      </section>
-
-      <section>
+      <section className={styles.secondary}>
         <Link href="/menu">Menu</Link>
         <Link href="/account">Account</Link>
-        <div>Cart</div>
+        <Link href="/cart">Cart</Link>
       </section>
     </nav>
   );
 }
 
-export { page };
+export default page;
