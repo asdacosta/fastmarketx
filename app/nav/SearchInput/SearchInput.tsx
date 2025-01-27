223accc452626a9df1c8f91c2c09383a3314dc2f
asdacosta <asdacosta18@gmail.com>
2025-01-27 07:56:56 +0000

    Add typing effect to placeholder

--- app/nav/SearchInput/SearchInput.tsx
+++ app/nav/SearchInput/SearchInput.tsx
@@ -1,9 +1,34 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./SearchInput.module.css";
+import Typed from "typed.js";
 
 function SearchInput() {
   const [searchValue, setSearchValue] = useState("");
+  const searchInputRef = useRef(null);
+
+  useEffect(() => {
+    if (!searchInputRef.current) return;
+
+    const typed = new Typed(searchInputRef.current, {
+      strings: [
+        "iphone",
+        "rice and chicken",
+        "Search products, services, or meals ^1500",
+      ],
+      attr: "placeholder",
+      loop: true,
+      loopCount: 10,
+      typeSpeed: 30,
+      backSpeed: 15,
+      startDelay: 1000,
+      backDelay: 2000,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(event.target.value);
@@ -15,20 +40,18 @@ function SearchInput() {
   };
 
   return (
-    <>
-      <form onSubmit={handleSearch} className={styles.form}>
-        <input
-          type="text"
-          placeholder="Search products or services"
-          aria-label="Search Product or Services"
-          value={searchValue}
-          onChange={handleSearchChange}
-        />
-        <button type="submit" aria-label="Submit search">
-          Search
-        </button>
-      </form>
-    </>
+    <form onSubmit={handleSearch} className={styles.form}>
+      <input
+        ref={searchInputRef}
+        type="text"
+        aria-label="Search Product or Services"
+        value={searchValue}
+        onChange={handleSearchChange}
+      />
+      <button type="submit" aria-label="Submit search">
+        Search
+      </button>
+    </form>
   );
 }
 
