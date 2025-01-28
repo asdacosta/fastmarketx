29ca5a07c055070aa2df002edf22d02bfdf1bb80
asdacosta <asdacosta18@gmail.com>
2025-01-28 15:04:43 +0000

    Refactor to define part in separate component

--- app/Main/Main.tsx
+++ app/Main/Main.tsx
@@ -1,53 +1,15 @@
 import React from "react";
 import styles from "./Main.module.css";
 import { Categories } from "./Categories/Categories";
-import Image from "next/image";
-import Link from "next/link";
 import { Extras } from "./Extras/Extras";
 import { Promotion } from "./Promotion/Promotion";
+import { CardsBox } from "./CardsBox/CardsBox";
 
 function Main() {
   return (
     <main className={styles.main}>
       <Categories />
-      <section className={styles.cardsBox}>
-        <section className={styles.card}>
-          <Image
-            src="/mainImgs/store.avif"
-            alt="Store"
-            fill
-            className={styles.img}
-          />
-          <span>Set up a store</span>
-        </section>
-        <section className={styles.card}>
-          <Image
-            src="/mainImgs/products.avif"
-            alt="Store"
-            fill
-            className={styles.img}
-          />
-          <span>Explore services</span>
-        </section>
-        <section className={styles.card}>
-          <Image
-            src="/mainImgs/services.avif"
-            alt="Store"
-            fill
-            className={styles.img}
-          />
-          <span>Discover Services</span>
-        </section>
-        <section className={styles.card}>
-          <Image
-            src="/mainImgs/foods.avif"
-            alt="Store"
-            fill
-            className={styles.img}
-          />
-          <span>Order meals</span>
-        </section>
-      </section>
+      <CardsBox />
       <section className={styles.extrasBox}>
         <Extras />
         <Promotion />
