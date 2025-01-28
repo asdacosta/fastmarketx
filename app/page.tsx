6e9b2f43a71c3ee7a5811168764243ec494824b5
asdacosta <asdacosta18@gmail.com>
2025-01-28 00:27:41 +0000

    Make use of components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Header } from "./Header/Header";
+import Main from "./Main/Main";
+import { Bot } from "./Bot/Bot";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>One real Costa</h1>
-      </main>
-      <footer className={styles.footer}></footer>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Bot />
+    </>
   );
 }
