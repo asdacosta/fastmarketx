4b6bbfe94ecb341ba33ef86cddf2b758e9b24864
asdacosta <asdacosta18@gmail.com>
2025-01-28 00:30:32 +0000

    Define Main component

---
+++ app/Main/Main.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./Main.module.css";
+import { Categories } from "./Categories/Categories";
+import Image from "next/image";
+import Link from "next/link";
+import { Extras } from "./Extras/Extras";
+import { Promotion } from "./Promotion/Promotion";
+
+function Main() {
+  return (
+    <main className={styles.main}>
+      <Categories />
+      <section className={styles.cardsBox}>
+        <section className={styles.card}>
+          <Image
+            src="/mainImgs/store.avif"
+            alt="Store"
+            layout="fill"
+            objectFit="fill"
+            className={styles.img}
+          />
+          <span>Set up a store</span>
+        </section>
+        <section className={styles.card}>
+          <Image
+            src="/mainImgs/products.avif"
+            alt="Store"
+            layout="fill"
+            objectFit="fill"
+            className={styles.img}
+          />
+          <span>Explore services</span>
+        </section>
+        <section className={styles.card}>
+          <Image
+            src="/mainImgs/services.avif"
+            alt="Store"
+            layout="fill"
+            objectFit="fill"
+            className={styles.img}
+          />
+          <span>Discover Services</span>
+        </section>
+        <section className={styles.card}>
+          <Image
+            src="/mainImgs/foods.avif"
+            alt="Store"
+            layout="fill"
+            objectFit="fill"
+            className={styles.img}
+          />
+          <span>Order meals</span>
+        </section>
+      </section>
+      <section className={styles.extrasBox}>
+        <Extras />
+        <Promotion />
+      </section>
+    </main>
+  );
+}
+
+export default Main;
