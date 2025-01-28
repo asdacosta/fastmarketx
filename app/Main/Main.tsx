833336c31fad3a8b20dad9d0be2eb4f5efac2e79
asdacosta <asdacosta18@gmail.com>
2025-01-28 15:37:03 +0000

    Transfer part to Welcome component

--- app/Main/Main.tsx
+++ app/Main/Main.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 import styles from "./Main.module.css";
-import Categories from "./Categories/Categories";
-import Extras from "./Extras/Extras";
-import Promotion from "./Promotion/Promotion";
-import CardsBox from "./CardsBox/CardsBox";
+import Welcome from "./Welcome/Welcome";
 
 function Main() {
   return (
     <main className={styles.main}>
-      <Categories />
-      <CardsBox />
-      <section className={styles.extrasBox}>
-        <Extras />
-        <Promotion />
-      </section>
+      <Welcome />
     </main>
   );
 }
