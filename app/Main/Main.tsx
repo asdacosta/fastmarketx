2f1e771657b0b1e0e7547fa464140055b02c8c2f
asdacosta <asdacosta18@gmail.com>
2025-01-28 21:06:43 +0000

    Add Main categories component

--- app/Main/Main.tsx
+++ app/Main/Main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Main.module.css";
 import Welcome from "./Welcome/Welcome";
+import MainCategories from "./MainCategories/MainCategories";
 
 function Main() {
   return (
     <main className={styles.main}>
       <Welcome />
+      <MainCategories />
     </main>
   );
 }
