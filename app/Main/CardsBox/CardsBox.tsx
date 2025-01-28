00b4544284de0dbc0692f6d5bd006d94b209e495
asdacosta <asdacosta18@gmail.com>
2025-01-28 15:05:18 +0000

    Define four cards

---
+++ app/Main/CardsBox/CardsBox.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "./CardsBox.module.css";
+import Image from "next/image";
+
+function CardsBox() {
+  return (
+    <section className={styles.cardsBox}>
+      <section className={styles.card}>
+        <Image
+          src="/mainImgs/store.avif"
+          alt="Store"
+          fill
+          className={styles.img}
+        />
+        <span>Set up a store</span>
+      </section>
+      <section className={styles.card}>
+        <Image
+          src="/mainImgs/products.avif"
+          alt="Store"
+          fill
+          className={styles.img}
+        />
+        <span>Explore services</span>
+      </section>
+      <section className={styles.card}>
+        <Image
+          src="/mainImgs/services.avif"
+          alt="Store"
+          fill
+          className={styles.img}
+        />
+        <span>Discover Services</span>
+      </section>
+      <section className={styles.card}>
+        <Image
+          src="/mainImgs/foods.avif"
+          alt="Store"
+          fill
+          className={styles.img}
+        />
+        <span>Order meals</span>
+      </section>
+    </section>
+  );
+}
+
+export { CardsBox };
