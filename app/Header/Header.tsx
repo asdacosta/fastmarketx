914ebbfaf6ab835205e0fc0d6bf6a1978b86244d
asdacosta <asdacosta18@gmail.com>
2025-01-28 00:28:31 +0000

    Define header component as a parent of other components

---
+++ app/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Nav } from "./Nav/Nav";
+import { InfoTab } from "./InfoTab/InfoTab";
+
+function Header() {
+  return (
+    <header>
+      <Nav />
+      <InfoTab />
+    </header>
+  );
+}
+
+export { Header };
