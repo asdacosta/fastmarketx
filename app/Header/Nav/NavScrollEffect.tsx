2d0d92ef8f92b50facd79200b37d6e62b0f0d955
asdacosta <asdacosta18@gmail.com>
2025-01-28 23:36:53 +0000

    Export to Nav component to hide it on downwards scroll and reveal otherwise

---
+++ app/Header/Nav/NavScrollEffect.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "./Nav.module.css";
+
+function NavScrollEffect() {
+  const [isHidden, setIsHidden] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      currentScrollY > lastScrollY && currentScrollY > 100
+        ? setIsHidden(true)
+        : setIsHidden(false);
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+  return (
+    <section
+      style={{ position: "absolute" }}
+      className={`${isHidden ? styles.hidden : ""}`}
+    ></section>
+  );
+}
+
+export default NavScrollEffect;
