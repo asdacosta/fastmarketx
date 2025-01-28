c93e52b91226eea3d72761a841ee033a684a4e83
asdacosta <asdacosta18@gmail.com>
2025-01-28 21:13:14 +0000

    Hide header on down scroll and reveal otherwise by listening to window

---
+++ app/Header/Nav/NavScrollEffect.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+function NavScrollEffect() {
+  const [isHidden, setIsHidden] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      currentScrollY > lastScrollY ? setIsHidden(true) : setIsHidden(false);
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
+  return isHidden;
+}
+
+export default NavScrollEffect;
