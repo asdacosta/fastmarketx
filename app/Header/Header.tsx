6c88a429a94b8a57c7aa4cd28a17135f665482be
asdacosta <asdacosta18@gmail.com>
2025-01-28 21:04:31 +0000

    Import styles

--- app/Header/Header.tsx
+++ app/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Nav from "./Nav/Nav";
 import InfoTab from "./InfoTab/InfoTab";
+import styles from "./Header.module.css";
 
 function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <Nav />
       <InfoTab />
     </header>
