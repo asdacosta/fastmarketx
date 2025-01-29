b47dce793b9e74a294919cdee102618a630cffa1
asdacosta <asdacosta18@gmail.com>
2025-01-29 23:12:38 +0000

    Render ads

---
+++ app/Main/Welcome/CardsBox/Ad/Ad.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import styles from "./Ad.module.css";
+import "./Swiper.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay, Pagination } from "swiper/modules";
+
+function Ad() {
+  const items = new Array(4).fill(1);
+
+  return (
+    <section className={styles.adBox}>
+      <Swiper
+        pagination={{ clickable: true }}
+        direction={"vertical"}
+        modules={[Pagination, Autoplay]}
+        slidesPerView={1}
+        slidesPerGroup={1}
+        loop
+        autoplay={{ delay: 10000, disableOnInteraction: false }}
+        className="mySwiper"
+      >
+        {items.map((item, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <section className={styles.item}>
+                <Image
+                  src="/mainImgs/products.avif"
+                  quality={80}
+                  draggable="false"
+                  alt="Store"
+                  fill
+                  className={styles.img}
+                />
+              </section>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+}
+
+export default Ad;
