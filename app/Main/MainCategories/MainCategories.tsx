873edf550e139d7643069a8755bbed42cf527e37
asdacosta <asdacosta18@gmail.com>
2025-01-29 07:44:57 +0000

    Use general template instead of different components

--- app/Main/MainCategories/MainCategories.tsx
+++ app/Main/MainCategories/MainCategories.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import styles from "./MainCategories.module.css";
-import MainProducts from "./MainProducts/MainProducts";
-import MainServices from "./MainServices/MainServices";
-import MainMeals from "./MainMeals/MainMeals";
-import MainStores from "./MainStores/MainStores";
 import MainTrends from "./MainTrends/MainTrends";
+import MainCategoryTemplate from "./MainCategoryTemplate/MainCategoryTemplate";
 
 function MainCategories() {
   return (
     <section className={styles.mainCategories}>
       <MainTrends />
-      <MainProducts />
-      <MainServices />
-      <MainMeals />
-      <MainStores />
+      <MainCategoryTemplate categoryName="Products" />
+      <MainCategoryTemplate categoryName="Services" />
+      <MainCategoryTemplate categoryName="Meals" />
+      <MainCategoryTemplate categoryName="Stores" />
     </section>
   );
 }
