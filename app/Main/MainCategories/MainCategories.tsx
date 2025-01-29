fb187d9508096584c733dc710c6b1f56f7ec62c8
asdacosta <asdacosta18@gmail.com>
2025-01-29 18:03:03 +0000

    Add extra components

--- app/Main/MainCategories/MainCategories.tsx
+++ app/Main/MainCategories/MainCategories.tsx
@@ -9,22 +9,27 @@ function MainCategories() {
   return (
     <section className={styles.mainCategories}>
       <MainItems itemsName="Trending Products" />
+      <MainItems itemsName="Speedy Deals" />
       <MainCategoryTemplate categoryName="Products" />
       <MainSubCategories categoryName="Products" />
       <MainItems itemsName="Popular Products" />
       <MainBrands />
+      <MainItems itemsName="Hot Deals" />
       <MainCategoryTemplate categoryName="Meals" />
       <MainSubCategories categoryName="Meals" />
       <MainItems itemsName="Popular Meals" />
       <MainBrands />
+      <MainItems itemsName="Hot Deals" />
       <MainCategoryTemplate categoryName="Services" />
       <MainSubCategories categoryName="Services" />
       <MainItems itemsName="Popular Services" />
       <MainBrands />
+      <MainItems itemsName="Hot Deals" />
       <MainCategoryTemplate categoryName="Stores" />
       <MainSubCategories categoryName="Stores" />
       <MainItems itemsName="Popular Stores" />
       <MainBrands />
+      <MainItems itemsName="Hot Deals" />
     </section>
   );
 }
