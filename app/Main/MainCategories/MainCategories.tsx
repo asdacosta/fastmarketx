94b43f12b1ffa50e1d0b98497f138a2abc808179
asdacosta <asdacosta18@gmail.com>
2025-01-29 18:26:59 +0000

    Parse value for newly defined props

--- app/Main/MainCategories/MainCategories.tsx
+++ app/Main/MainCategories/MainCategories.tsx
@@ -9,27 +9,28 @@ function MainCategories() {
   return (
     <section className={styles.mainCategories}>
       <MainItems itemsName="Trending Products" />
-      <MainItems itemsName="Speedy Deals" />
+      <MainItems itemsName="Speedy Deals" itemsQuantity={7} />
+      <MainItems itemsName="Low Cost" itemsQuantity={7} />
       <MainCategoryTemplate categoryName="Products" />
       <MainSubCategories categoryName="Products" />
       <MainItems itemsName="Popular Products" />
       <MainBrands />
-      <MainItems itemsName="Hot Deals" />
+      <MainItems itemsName="Hot Deals" itemsQuantity={7} />
       <MainCategoryTemplate categoryName="Meals" />
       <MainSubCategories categoryName="Meals" />
       <MainItems itemsName="Popular Meals" />
       <MainBrands />
-      <MainItems itemsName="Hot Deals" />
+      <MainItems itemsName="Hot Deals" itemsQuantity={7} />
       <MainCategoryTemplate categoryName="Services" />
       <MainSubCategories categoryName="Services" />
       <MainItems itemsName="Popular Services" />
       <MainBrands />
-      <MainItems itemsName="Hot Deals" />
+      <MainItems itemsName="Hot Deals" itemsQuantity={7} />
       <MainCategoryTemplate categoryName="Stores" />
       <MainSubCategories categoryName="Stores" />
       <MainItems itemsName="Popular Stores" />
       <MainBrands />
-      <MainItems itemsName="Hot Deals" />
+      <MainItems itemsName="Hot Deals" itemsQuantity={7} />
     </section>
   );
 }
