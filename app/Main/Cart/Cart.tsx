ba9c58f085bd5f84b678c1ba1d1fdc61771865b8
asdacosta <asdacosta18@gmail.com>
2025-02-01 16:35:35 +0000

    Refactor part into separate components

--- app/Main/Cart/Cart.tsx
+++ app/Main/Cart/Cart.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./Cart.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import EmptyCart from "../EmptyCart/EmptyCart";
+import CategoryCart from "./CategoriesCart/CategoriesCart";
 
 function Cart() {
   return (
@@ -21,115 +23,8 @@ function Cart() {
           <span className={styles.itemsCount}>Items | 10</span>
         </section>
         <section className={styles.items}>
-          <section className={styles.emptyList}>
-            <span>Cart is empty</span>
-            <Link href="/" className={styles.addItems}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
-              </svg>
-              <span>Shop special deals</span>
-            </Link>
-            <Link href="/" className={styles.addItems}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                <path d="M543.8 287.6c17 0 32-14 32-32.1c1-9-3-17-11-24L309.5 7c-6-5-14-7-21-7s-15 1-22 8L10 231.5c-7 7-10 15-10 24c0 18 14 32.1 32 32.1l32 0 0 160.4c0 35.3 28.7 64 64 64l102.3 0-31.3-52.2c-4.1-6.8-2.6-15.5 3.5-20.5L288 368l-60.2-82.8c-10.9-15 8.2-33.5 22.8-22l117.9 92.6c8 6.3 8.2 18.4 .4 24.9L288 448l38.4 64 122.1 0c35.5 0 64.2-28.8 64-64.3l-.7-160.2 32 0z" />
-              </svg>
-              <span>Home</span>
-            </Link>
-            <Link href="/" className={styles.addItems}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM215 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L392 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-214.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L103 273c-9.4-9.4-9.4-24.6 0-33.9L215 127z" />
-              </svg>
-              <span>Return</span>
-            </Link>
-          </section>
-          <section className={styles.productsList}>
-            <h4>Products</h4>
-            <section className={styles.list}>
-              <section className={styles.item}>
-                <div>
-                  <Image src="/mainImgs/store.avif" alt="Som" fill />
-                </div>
-                <div className={styles.itemInfo}>
-                  <div className={styles.namePrice}>
-                    <span className={styles.itemName}>
-                      Product name is here
-                    </span>
-                    <div className={styles.price}>
-                      <span>GH₵</span>
-                      <span>1000.45</span>
-                    </div>
-                  </div>
-                  <div className={styles.accountStock}>
-                    <span className={styles.accountName}>Account Name</span>
-                    <span>•</span>
-                    <span className={styles.stock}>In Stock</span>
-                  </div>
-                  <div className={styles.itemQuantity}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 448 512"
-                    >
-                      <path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zm88 200l144 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-144 0c-13.3 0-24-10.7-24-24s10.7-24 24-24z" />
-                    </svg>
-                    <span>1</span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 448 512"
-                    >
-                      <path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM200 344l0-64-64 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l64 0 0-64c0-13.3 10.7-24 24-24s24 10.7 24 24l0 64 64 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-64 0 0 64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" />
-                    </svg>
-                  </div>
-                  <div className={styles.removeSave}>
-                    <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 448 512"
-                        width="0.65rem"
-                      >
-                        <path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z" />
-                      </svg>
-                      <span>Remove</span>
-                    </div>
-                    <div>•</div>
-                    <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512"
-                        width="0.85rem"
-                      >
-                        <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
-                      </svg>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512"
-                        width="0.85rem"
-                      >
-                        <path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z" />
-                      </svg>
-                      <span>Wishlist</span>
-                    </div>
-                  </div>
-                </div>
-              </section>
-            </section>
-            <Link href="/" className={styles.addItems}>
-              Shop more
-            </Link>
-          </section>
-          <section className={styles.mealsList}>
-            <h4>Meals</h4>
-            <section className={styles.list}></section>
-            <Link href="/" className={styles.addItems}>
-              Shop more
-            </Link>
-          </section>
-          <section className={styles.servicesList}>
-            <h4>Services</h4>
-            <section className={styles.list}></section>
-            <Link href="/" className={styles.addItems}>
-              Shop more
-            </Link>
-          </section>
+          <EmptyCart />
+          <CategoryCart />
         </section>
       </section>
       <section className={styles.cartSummary}>
