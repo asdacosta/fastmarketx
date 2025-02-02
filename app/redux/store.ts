024324e768348296c4b6bb19f624fd2f90d5c530
asdacosta <asdacosta18@gmail.com>
2025-02-02 12:30:04 +0000

    Add cart display reducer

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
 import cartReducer from "./slices/cartSlice";
 import wishlistReducer from "./slices/wishlistSlice";
+import cartDisplayReducer from "./slices/cartDisplaySlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     cart: cartReducer,
     wishlist: wishlistReducer,
+    cartDisplay: cartDisplayReducer,
   },
 });
 
