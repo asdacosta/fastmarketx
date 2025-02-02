8edc8fca4c7c51569ee6f5abed5212cb62c4403a
asdacosta <asdacosta18@gmail.com>
2025-02-02 12:29:49 +0000

    Display cart depending on state in cart store

--- app/Main/Main.tsx
+++ app/Main/Main.tsx
@@ -2,12 +2,26 @@ import React from "react";
 import styles from "./Main.module.css";
 import Welcome from "./Welcome/Welcome";
 import MainCategories from "./MainCategories/MainCategories";
+import Cart from "./Cart/Cart";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 function Main() {
+  const cartOpen = useSelector(
+    (state: RootState) => state.cartDisplay.cartOpen
+  );
+
   return (
     <main className={styles.main}>
-      <Welcome />
-      <MainCategories />
+      {cartOpen ? (
+        <Cart />
+      ) : (
+        <>
+          {" "}
+          <Welcome />
+          <MainCategories />
+        </>
+      )}
     </main>
   );
 }
