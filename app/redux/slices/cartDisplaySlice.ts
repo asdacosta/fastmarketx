b9c81ee983b3c87b57ce58dde391cea43b9b0e93
asdacosta <asdacosta18@gmail.com>
2025-02-02 12:30:44 +0000

    Define reducer to toggle display of cart

---
+++ app/redux/slices/cartDisplaySlice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState: { cartOpen: boolean } = {
+  cartOpen: false,
+};
+
+const cartDisplaySlice = createSlice({
+  name: "cartUi",
+  initialState,
+  reducers: {
+    toggleCart: (state) => {
+      state.cartOpen = !state.cartOpen;
+    },
+    openCart: (state) => {
+      state.cartOpen = true;
+    },
+    closeCart: (state) => {
+      state.cartOpen = false;
+    },
+  },
+});
+
+export const { toggleCart, openCart, closeCart } = cartDisplaySlice.actions;
+export default cartDisplaySlice.reducer;
