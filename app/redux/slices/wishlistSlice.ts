b8805213daefc314ae972f34589c739b5a3e9839
asdacosta <asdacosta18@gmail.com>
2025-02-02 12:31:39 +0000

    Use descriptive names

--- app/redux/slices/wishlistSlice.ts
+++ app/redux/slices/wishlistSlice.ts
@@ -18,18 +18,19 @@ const wishlistSlice = createSlice({
   name: "wishlist",
   initialState,
   reducers: {
-    add: (state, action: PayloadAction<WishlistItem>) => {
+    addToWishlist: (state, action: PayloadAction<WishlistItem>) => {
       const exists = state.items.some((item) => item.id === action.payload.id);
       if (exists) state.items.push(action.payload);
     },
-    remove: (state, action: PayloadAction<String>) => {
+    removeFromWishlist: (state, action: PayloadAction<String>) => {
       state.items = state.items.filter((item) => item.id !== action.payload);
     },
-    clear: (state) => {
+    clearWishlist: (state) => {
       state.items = [];
     },
   },
 });
 
-export const { add, remove, clear } = wishlistSlice.actions;
+export const { addToWishlist, removeFromWishlist, clearWishlist } =
+  wishlistSlice.actions;
 export default wishlistSlice.reducer;
