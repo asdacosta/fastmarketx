fc23b760bd38b47ecc4aab8aa9b74d34a2466344
asdacosta <asdacosta18@gmail.com>
2025-02-02 11:27:25 +0000

    Provide redux store to components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
+"use client";
+
 import styles from "./page.module.css";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Bot from "./Bot/Bot";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <Main />
-      <Bot />
+      <Provider store={store}>
+        <Header />
+        <Main />
+        <Bot />
+      </Provider>
     </>
   );
 }
