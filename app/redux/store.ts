7f61924aaeb75b843e08f2df686e2f56661c4913
asdacosta <asdacosta18@gmail.com>
2025-02-03 14:36:17 +0000

    Add menu display reducer

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -3,6 +3,7 @@ import userReducer from "./slices/userSlice";
 import cartReducer from "./slices/cartSlice";
 import wishlistReducer from "./slices/wishlistSlice";
 import cartDisplayReducer from "./slices/cartDisplaySlice";
+import menuDisplayReducer from "./slices/MenuDisplaySlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ export const store = configureStore({
     cart: cartReducer,
     wishlist: wishlistReducer,
     cartDisplay: cartDisplayReducer,
+    menuDisplay: menuDisplayReducer,
   },
 });
 
