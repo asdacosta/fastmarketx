7e4b38cea0d91a845565c17b41cc21cf60f41078
asdacosta <asdacosta18@gmail.com>
2025-02-03 15:25:25 +0000

    Replace with generalized component in charge of all display

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -2,16 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
 import cartReducer from "./slices/cartSlice";
 import wishlistReducer from "./slices/wishlistSlice";
-import cartDisplayReducer from "./slices/cartDisplaySlice";
-import menuDisplayReducer from "./slices/MenuDisplaySlice";
+import componentDisplayReducer from "./slices/componentDisplaySlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     cart: cartReducer,
     wishlist: wishlistReducer,
-    cartDisplay: cartDisplayReducer,
-    menuDisplay: menuDisplayReducer,
+    componentDisplay: componentDisplayReducer,
   },
 });
 
