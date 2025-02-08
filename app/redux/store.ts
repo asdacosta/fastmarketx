4ebec10b2851c4f43087f4ca587b4a23fcfd5909
asdacosta <asdacosta18@gmail.com>
2025-02-08 01:19:17 +0000

    Add create store form state management reducer

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -3,6 +3,7 @@ import userReducer from "./slices/userSlice";
 import cartReducer from "./slices/cartSlice";
 import wishlistReducer from "./slices/wishlistSlice";
 import componentDisplayReducer from "./slices/componentDisplaySlice";
+import createStoreFormSlice from "./slices/CreateStoreFormSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ export const store = configureStore({
     cart: cartReducer,
     wishlist: wishlistReducer,
     componentDisplay: componentDisplayReducer,
+    createStoreForm: createStoreFormSlice,
   },
 });
 
