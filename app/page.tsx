3c02fbcaacb7fe82eb7605ca051691fdbaf57bf8
asdacosta <asdacosta18@gmail.com>
2025-02-08 21:48:33 +0000

    Refactor to turn page to server side component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,14 @@
-"use client";
-
 import styles from "./page.module.css";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Bot from "./Bot/Bot";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
 
 export default function Home() {
   return (
     <>
-      <Provider store={store}>
-        <Header />
-        <Main />
-        <Bot />
-      </Provider>
+      <Header />
+      <Main />
+      <Bot />
     </>
   );
 }
