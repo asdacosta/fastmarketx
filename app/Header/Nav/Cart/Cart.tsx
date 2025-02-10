47b56fc2a8d5deb0c412f1676b4af331b10637e8
asdacosta <asdacosta18@gmail.com>
2025-02-10 21:42:39 +0000

    Make use of server rendering instead of client

--- app/Header/Nav/Cart/Cart.tsx
+++ app/Header/Nav/Cart/Cart.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import styles from "./Cart.module.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "@/app/redux/store";
-import { toggleCart } from "@/app/redux/slices/componentDisplaySlice";
+import Link from "next/link";
 
 function Cart() {
-  const dispatch = useDispatch();
   const cartItems = useSelector((state: RootState) => state.cart.items);
 
   return (
     <section>
-      <div className={styles.cart} onClick={() => dispatch(toggleCart())}>
+      <Link href="/cart" className={styles.cart}>
         <span className={styles.cartNum}>
           {cartItems.reduce((current, acc) => current + acc.quantity, 0)}
         </span>
@@ -18,7 +17,7 @@ function Cart() {
           <path d="M423.3 440.7c0 25.3-20.3 45.6-45.6 45.6s-45.8-20.3-45.8-45.6 20.6-45.8 45.8-45.8c25.4 0 45.6 20.5 45.6 45.8zm-253.9-45.8c-25.3 0-45.6 20.6-45.6 45.8s20.3 45.6 45.6 45.6 45.8-20.3 45.8-45.6-20.5-45.8-45.8-45.8zm291.7-270C158.9 124.9 81.9 112.1 0 25.7c34.4 51.7 53.3 148.9 373.1 144.2 333.3-5 130 86.1 70.8 188.9 186.7-166.7 319.4-233.9 17.2-233.9z" />
         </svg>
         <span>Cart</span>
-      </div>
+      </Link>
     </section>
   );
 }
