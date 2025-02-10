022e54e81cbcfda7a46ea227babbff343a370630
asdacosta <asdacosta18@gmail.com>
2025-02-10 21:44:23 +0000

    Remove trivial reducer

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
 import cartReducer from "./slices/cartSlice";
 import wishlistReducer from "./slices/wishlistSlice";
-import componentDisplayReducer from "./slices/componentDisplaySlice";
 import createStoreFormSlice from "./slices/CreateStoreFormSlice";
 
 export const store = configureStore({
@@ -10,7 +9,6 @@ export const store = configureStore({
     user: userReducer,
     cart: cartReducer,
     wishlist: wishlistReducer,
-    componentDisplay: componentDisplayReducer,
     createStoreForm: createStoreFormSlice,
   },
 });
