151f11a1da19753a337d398a141f65aac3a78274
asdacosta <asdacosta18@gmail.com>
2025-02-10 21:43:14 +0000

    Remove components that have been made to render on server side

--- app/Main/Main.tsx
+++ app/Main/Main.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import styles from "./Main.module.css";
 import Welcome from "./Welcome/Welcome";
 import MainCategories from "./MainCategories/MainCategories";
-import CartClient from "./Cart/CartClient";
-import MenuClient from "./Menu/MenuClient";
 
 function Main() {
   return (
     <main className={styles.main}>
-      <CartClient />
-      <MenuClient />
       <Welcome />
       <MainCategories />
     </main>
