6eab51cf8ac0758c7941a7a748b19e00c6215217
asdacosta <asdacosta18@gmail.com>
2025-02-17 23:10:51 +0000

    Add section to allow increment or decrement of item quantity

--- app/Main/MainCategories/Item/Item.tsx
+++ app/Main/MainCategories/Item/Item.tsx
@@ -5,6 +5,20 @@ import Image from "next/image";
 
 function Item() {
   const [addedToWish, setAddedToWish] = useState(false);
+  const [addedToCart, setAddedToCart] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+
+  const decrementQuantity = () => {
+    setQuantity((prev) => {
+      if (prev === 1) {
+        setAddedToCart(false);
+        return prev;
+      }
+      return prev - 1;
+    });
+  };
+
+  const incrementQuantity = () => setQuantity((prev) => prev + 1);
 
   return (
     <section className={styles.item}>
@@ -67,7 +81,21 @@ function Item() {
           </svg>
           <span>Official Store</span>
         </div>
-        <button className={styles.add}>Add to cart</button>
+        {!addedToCart ? (
+          <button className={styles.add} onClick={() => setAddedToCart(true)}>
+            Add to cart
+          </button>
+        ) : (
+          <div className={styles.addQuantity}>
+            <svg viewBox="0 0 448 512" onClick={decrementQuantity}>
+              <path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zm88 200l144 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-144 0c-13.3 0-24-10.7-24-24s10.7-24 24-24z" />
+            </svg>
+            <span>{quantity}</span>
+            <svg viewBox="0 0 448 512" onClick={incrementQuantity}>
+              <path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM200 344l0-64-64 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l64 0 0-64c0-13.3 10.7-24 24-24s24 10.7 24 24l0 64 64 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-64 0 0 64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" />
+            </svg>
+          </div>
+        )}
       </div>
     </section>
   );
