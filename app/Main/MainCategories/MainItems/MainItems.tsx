fe33e34d90ea505f56fcaf89c2278dcfafeffa39
asdacosta <asdacosta18@gmail.com>
2025-02-18 07:12:28 +0000

    Refactor part to solitary component: Item

--- app/Main/MainCategories/MainItems/MainItems.tsx
+++ app/Main/MainCategories/MainItems/MainItems.tsx
@@ -1,7 +1,7 @@
 import React, { JSX } from "react";
 import styles from "./MainItems.module.css";
-import Image from "next/image";
 import Link from "next/link";
+import Item from "../Item/Item";
 
 interface MainItemsProps {
   itemsName: string;
@@ -52,33 +52,7 @@ function MainItems({ itemsName, itemsQuantity = 14 }: MainItemsProps) {
       <section className={styles.trendItems}>
         {items.map((item, index) => {
           return (
-            <section key={index} className={styles.item}>
-              <div className={styles.imgBox}>
-                <Image
-                  draggable="false"
-                  src="/mainImgs/products.avif"
-                  alt="Store"
-                  fill
-                  className={styles.img}
-                />
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                  <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336l24 0 0-64-24 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l48 0c13.3 0 24 10.7 24 24l0 88 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-80 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
-                </svg>
-              </div>
-              <div className={styles.productInfo}>
-                <span className={styles.productName}>Product Name</span>
-                <div className={styles.price}>
-                  <span>GH₵</span>
-                  <span>100.45</span>
-                </div>
-                <div className={styles.storeName}>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                    <path d="M309.5 13.5C305.5 5.2 297.1 0 287.9 0s-17.6 5.2-21.6 13.5L197.7 154.8 44.5 177.5c-9 1.3-16.5 7.6-19.3 16.3s-.5 18.1 5.9 24.5L142.2 328.4 116 483.9c-1.5 9 2.2 18.1 9.7 23.5s17.3 6 25.3 1.7l137-73.2 137 73.2c8.1 4.3 17.9 3.7 25.3-1.7s11.2-14.5 9.7-23.5L433.6 328.4 544.8 218.2c6.5-6.4 8.7-15.9 5.9-24.5s-10.3-14.9-19.3-16.3L378.1 154.8 309.5 13.5zM288 384.7l0-305.6 52.5 108.1c3.5 7.1 10.2 12.1 18.1 13.3l118.3 17.5L391 303c-5.5 5.5-8.1 13.3-6.8 21l20.2 119.6L299.2 387.5c-3.5-1.9-7.4-2.8-11.2-2.8z" />
-                  </svg>
-                  <span>Official Store</span>
-                </div>
-              </div>
-            </section>
+            <Item key={index} includeButton={false} includeStars={false} />
           );
         })}
       </section>
