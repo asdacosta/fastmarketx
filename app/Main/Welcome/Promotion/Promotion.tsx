4f7c3e48ce0f090550169017e10571514535240d
asdacosta <asdacosta18@gmail.com>
2025-02-18 07:30:26 +0000

    Add Item component

--- app/Main/Welcome/Promotion/Promotion.tsx
+++ app/Main/Welcome/Promotion/Promotion.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./Promotion.module.css";
+import Item from "../../MainCategories/Item/Item";
 
 function Promotion() {
   return (
     <section className={styles.promotion}>
       <section>
         <h2>Today's Special Offer</h2>
+      </section>
+      <section className={styles.item}>
+        <Item />
       </section>
     </section>
   );
