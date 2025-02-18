0e00352adae6bb89902e8801bcbcfe551e23d835
asdacosta <asdacosta18@gmail.com>
2025-02-18 09:01:13 +0000

    Allow single item instead of items

--- app/ItemTemplate/ItemTemplate.tsx
+++ app/ItemTemplate/ItemTemplate.tsx
@@ -15,44 +15,36 @@ function ItemTemplate({
   buttons = [],
   price = true,
 }: ItemTemplateProps) {
-  const items = new Array(itemsQuantity).fill(1);
-
   return (
-    <section className={styles.items}>
-      {items.map((_, index) => {
-        return (
-          <section key={index} className={styles.item}>
-            <div className={styles.imgBox}>
-              <Image
-                draggable="false"
-                src="/mainImgs/products.avif"
-                alt="Store"
-                fill
-                className={styles.img}
-              />
-            </div>
-            <div className={styles.productInfo}>
-              <span className={styles.productName}>Product Name</span>
-              {price && (
-                <div className={styles.price}>
-                  <span>GH₵</span>
-                  <span>100.45</span>
-                </div>
-              )}
-              {extraDetails.map((detail, index) => (
-                <span key={index} className={styles.extraDetail}>
-                  {detail}
-                </span>
-              ))}
-            </div>
-            <div className={styles.buttons}>
-              {buttons.map((content, index) => {
-                return <button key={index}>{content}</button>;
-              })}
-            </div>
-          </section>
-        );
-      })}
+    <section className={styles.item}>
+      <div className={styles.imgBox}>
+        <Image
+          draggable="false"
+          src="/mainImgs/products.avif"
+          alt="Store"
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.productInfo}>
+        <span className={styles.productName}>Product Name</span>
+        {price && (
+          <div className={styles.price}>
+            <span>GH₵</span>
+            <span>100.45</span>
+          </div>
+        )}
+        {extraDetails.map((detail, index) => (
+          <span key={index} className={styles.extraDetail}>
+            {detail}
+          </span>
+        ))}
+      </div>
+      <div className={styles.buttons}>
+        {buttons.map((content, index) => {
+          return <button key={index}>{content}</button>;
+        })}
+      </div>
     </section>
   );
 }
