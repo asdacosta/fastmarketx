e3f4380a812afa489820518a889c15099d38f17a
asdacosta <asdacosta18@gmail.com>
2025-02-20 09:07:05 +0000

    Define store logo div with cloned svg and colors

--- app/Main/MainCategories/Item/Item.tsx
+++ app/Main/MainCategories/Item/Item.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import styles from "./Item.module.css";
 import Image from "next/image";
+import { storeIcons } from "./StoreIcons";
 
 interface ItemProps {
   includeStars?: boolean;
@@ -18,6 +19,9 @@ function Item({
   const [addedToCart, setAddedToCart] = useState(false);
   const [quantity, setQuantity] = useState(1);
   const [revealDescription, setRevealDescription] = useState(false);
+
+  const [storeColor, setStoreColor] = useState("rgba(232, 205, 4, 1)");
+  const [storeIconIndex, setStoreIconIndex] = useState(10);
 
   const decrementQuantity = () => {
     setQuantity((prev) => {
@@ -106,9 +110,21 @@ function Item({
           </div>
         )}
         <div className={styles.storeName}>
-          <svg viewBox="0 0 576 512">
-            <path d="M309.5 13.5C305.5 5.2 297.1 0 287.9 0s-17.6 5.2-21.6 13.5L197.7 154.8 44.5 177.5c-9 1.3-16.5 7.6-19.3 16.3s-.5 18.1 5.9 24.5L142.2 328.4 116 483.9c-1.5 9 2.2 18.1 9.7 23.5s17.3 6 25.3 1.7l137-73.2 137 73.2c8.1 4.3 17.9 3.7 25.3-1.7s11.2-14.5 9.7-23.5L433.6 328.4 544.8 218.2c6.5-6.4 8.7-15.9 5.9-24.5s-10.3-14.9-19.3-16.3L378.1 154.8 309.5 13.5zM288 384.7l0-305.6 52.5 108.1c3.5 7.1 10.2 12.1 18.1 13.3l118.3 17.5L391 303c-5.5 5.5-8.1 13.3-6.8 21l20.2 119.6L299.2 387.5c-3.5-1.9-7.4-2.8-11.2-2.8z" />
-          </svg>
+          <div
+            className={`${styles.storeLogo} ${styles.officialColor} ${styles.officialIcon}`}
+            style={{ background: storeColor }}
+          >
+            {React.cloneElement(storeIcons.stores[storeIconIndex], {
+              fill: "black",
+              width: 16,
+              height: 16,
+              stroke: "black",
+              strokeWidth: 10,
+              style: {
+                filter: `drop-shadow(2px 2px 5px #000)`,
+              },
+            })}
+          </div>
           <span>Official Store</span>
         </div>
         {includeButton && (
