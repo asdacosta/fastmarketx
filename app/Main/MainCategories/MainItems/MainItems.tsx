df78c564bcce91df939c84f9409b7b70f6a06e10
asdacosta <asdacosta18@gmail.com>
2025-02-20 08:11:38 +0000

    Add category props that takes three particular strings only

--- app/Main/MainCategories/MainItems/MainItems.tsx
+++ app/Main/MainCategories/MainItems/MainItems.tsx
@@ -6,35 +6,40 @@ import Item from "../Item/Item";
 interface MainItemsProps {
   itemsName: string;
   itemsQuantity?: number;
+  category?: "product" | "meal" | "service";
 }
 
-function MainItems({ itemsName, itemsQuantity = 14 }: MainItemsProps) {
+function MainItems({
+  itemsName,
+  itemsQuantity = 14,
+  category = "product",
+}: MainItemsProps) {
   const items = new Array(itemsQuantity).fill(1);
   const initials = itemsName.split(" ")[0];
 
   const itemsSvg: { [key: string]: JSX.Element } = {
     Low: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <svg viewBox="0 0 512 512">
         <path d="M345 39.1L472.8 168.4c52.4 53 52.4 138.2 0 191.2L360.8 472.9c-9.3 9.4-24.5 9.5-33.9 .2s-9.5-24.5-.2-33.9L438.6 325.9c33.9-34.3 33.9-89.4 0-123.7L310.9 72.9c-9.3-9.4-9.2-24.6 .2-33.9s24.6-9.2 33.9 .2zM0 229.5L0 80C0 53.5 21.5 32 48 32l149.5 0c17 0 33.3 6.7 45.3 18.7l168 168c25 25 25 65.5 0 90.5L277.3 442.7c-25 25-65.5 25-90.5 0l-168-168C6.7 262.7 0 246.5 0 229.5zM144 144a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" />
       </svg>
     ),
     Trending: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <svg viewBox="0 0 512 512">
         <path d="M64 64c0-17.7-14.3-32-32-32S0 46.3 0 64L0 400c0 44.2 35.8 80 80 80l400 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 416c-8.8 0-16-7.2-16-16L64 64zm406.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L320 210.7l-57.4-57.4c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L240 221.3l57.4 57.4c12.5 12.5 32.8 12.5 45.3 0l128-128z" />
       </svg>
     ),
     Popular: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+      <svg viewBox="0 0 448 512">
         <path d="M160 80c0-26.5 21.5-48 48-48l32 0c26.5 0 48 21.5 48 48l0 352c0 26.5-21.5 48-48 48l-32 0c-26.5 0-48-21.5-48-48l0-352zM0 272c0-26.5 21.5-48 48-48l32 0c26.5 0 48 21.5 48 48l0 160c0 26.5-21.5 48-48 48l-32 0c-26.5 0-48-21.5-48-48L0 272zM368 96l32 0c26.5 0 48 21.5 48 48l0 288c0 26.5-21.5 48-48 48l-32 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48z" />
       </svg>
     ),
     Speedy: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+      <svg viewBox="0 0 384 512">
         <path d="M0 256L28.5 28c2-16 15.6-28 31.8-28H228.9c15 0 27.1 12.1 27.1 27.1c0 3.2-.6 6.5-1.7 9.5L208 160H347.3c20.2 0 36.7 16.4 36.7 36.7c0 7.4-2.2 14.6-6.4 20.7l-192.2 281c-5.9 8.6-15.6 13.7-25.9 13.7h-2.9c-15.7 0-28.5-12.8-28.5-28.5c0-2.3 .3-4.6 .9-6.9L176 288H32c-17.7 0-32-14.3-32-32z" />
       </svg>
     ),
     Hot: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+      <svg viewBox="0 0 448 512">
         <path d="M159.3 5.4c7.8-7.3 19.9-7.2 27.7 .1c27.6 25.9 53.5 53.8 77.7 84c11-14.4 23.5-30.1 37-42.9c7.9-7.4 20.1-7.4 28 .1c34.6 33 63.9 76.6 84.5 118c20.3 40.8 33.8 82.5 33.8 111.9C448 404.2 348.2 512 224 512C98.4 512 0 404.1 0 276.5c0-38.4 17.8-85.3 45.4-131.7C73.3 97.7 112.7 48.6 159.3 5.4zM225.7 416c25.3 0 47.7-7 68.8-21c42.1-29.4 53.4-88.2 28.1-134.4c-4.5-9-16-9.6-22.5-2l-25.2 29.3c-6.6 7.6-18.5 7.4-24.7-.5c-16.5-21-46-58.5-62.8-79.8c-6.3-8-18.3-8.1-24.7-.1c-33.8 42.5-50.8 69.3-50.8 99.4C112 375.4 162.6 416 225.7 416z" />
       </svg>
     ),
@@ -52,7 +57,12 @@ function MainItems({ itemsName, itemsQuantity = 14 }: MainItemsProps) {
       <section className={styles.trendItems}>
         {items.map((item, index) => {
           return (
-            <Item key={index} includeButton={false} includeStars={false} />
+            <Item
+              key={index}
+              includeButton={false}
+              includeStars={false}
+              category={category}
+            />
           );
         })}
       </section>
