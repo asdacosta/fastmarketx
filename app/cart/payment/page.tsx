ed95bc30bbd681209156ca5ca86c300ad14245fd
asdacosta <asdacosta18@gmail.com>
2025-02-21 19:43:32 +0000

    Add payment page to make user add location address and select payment methods

---
+++ app/cart/payment/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Header from "@/app/Header/Header";
+import Link from "next/link";
+import LocationAddress from "./LocationAddress/LocationAddress";
+import CartSummary from "./CartSummary/CartSummary";
+import Payment from "./Payment/Payment";
+
+function Page() {
+  const [receivingMethod, setReceivingMethod] = useState("payNow");
+
+  return (
+    <>
+      <Header />
+      <section className={styles.page}>
+        <section className={styles.paymentInfo}>
+          <LocationAddress />
+          <div className={styles.paymentMethod}>
+            <h2>SELECT PAYMENT METHOD (Door Delivery)</h2>
+            <div className={styles.methods}>
+              <label
+                className={`${styles.radioLabel} ${
+                  receivingMethod === "payNow" && styles.active
+                }`}
+              >
+                <input
+                  type="radio"
+                  value="payNow"
+                  checked={receivingMethod === "payNow"}
+                  onChange={(e) => setReceivingMethod(e.target.value)}
+                  className={styles.radioInput}
+                />
+                <span className={styles.customRadio}></span>
+                Pay now (Recommended)
+                <span className={styles.details}>
+                  Pay for delivery fee and items cost now
+                </span>
+              </label>
+              <label
+                className={`${styles.radioLabel} ${
+                  receivingMethod === "payOnDelivery" && styles.active
+                }`}
+              >
+                <input
+                  type="radio"
+                  value="payOnDelivery"
+                  checked={receivingMethod === "payOnDelivery"}
+                  onChange={(e) => setReceivingMethod(e.target.value)}
+                  className={styles.radioInput}
+                />
+                <span className={styles.customRadio}></span>
+                Pay on delivery
+                <span className={styles.details}>
+                  Pay for delivery fee now and pay for the items upon delivery
+                </span>
+              </label>
+            </div>
+          </div>
+          <Payment />
+        </section>
+        <CartSummary />
+      </section>
+    </>
+  );
+}
+
+export default Page;
