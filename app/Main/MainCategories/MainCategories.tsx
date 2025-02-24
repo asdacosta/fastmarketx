54094a01dbe3c50d99d2bc46f409f0bcdb9fb97d
asdacosta <asdacosta18@gmail.com>
2025-02-24 18:52:48 +0000

    Specify props for respective components

--- app/Main/MainCategories/MainCategories.tsx
+++ app/Main/MainCategories/MainCategories.tsx
@@ -8,33 +8,185 @@ import MainBrands from "./MainBrands/MainBrands";
 function MainCategories() {
   return (
     <section className={styles.mainCategories}>
-      <MainItems itemsName="Trending Products" />
-      <MainItems itemsName="Speedy Deals" itemsQuantity={7} />
-      <MainItems itemsName="Low Cost" itemsQuantity={7} />
-      <MainCategoryTemplate categoryName="Products" />
-      <MainSubCategories categoryName="Products" />
-      <MainItems itemsName="Popular Products" itemsQuantity={7} />
+      <MainItems itemsName="Trending Products" url="/products/trending" />
+      <MainItems
+        itemsName="Speedy Deals"
+        itemsQuantity={7}
+        url="/products/speedy"
+      />
+      <MainItems
+        itemsName="Low Cost"
+        itemsQuantity={7}
+        url="/products/lowcost"
+      />
+      <MainCategoryTemplate
+        categoryName="Products"
+        subCategories={[
+          "electronics",
+          "groceries",
+          "fashion",
+          "academic",
+          "homeandkitchen",
+          "beautyandhealth",
+          "sports",
+          "gamesandtoys",
+          "automotive",
+        ]}
+      />
+      <MainSubCategories
+        categoryName="Products"
+        subCategoryData={{
+          Fashion: ["Clothing", "Footwear", "Accessories", "Bags"],
+          Electronics: [
+            "Phones & Accessories",
+            "Computers & Tablets",
+            "Audio & Photography",
+            "Home Appliances",
+          ],
+          Groceries: [
+            "Fresh Foods",
+            "Kitchen Essentials",
+            "Snacks & Sweet",
+            "Drinks & Beverages",
+          ],
+          "Home & Kitchen": [
+            "Furniture",
+            "Home Decor",
+            "Kitchen & Dining",
+            "Bed & Bath",
+          ],
+        }}
+      />
+      <MainItems
+        itemsName="Popular Products"
+        itemsQuantity={7}
+        url="/products/popular"
+      />
       <MainBrands />
-      <MainItems itemsName="Hot Deals" itemsQuantity={7} />
-      <MainCategoryTemplate categoryName="Meals" />
-      <MainSubCategories categoryName="Meals" />
-      <MainItems itemsName="Popular Meals" itemsQuantity={7} category="meal" />
+      <MainItems
+        itemsName="Hot Deals"
+        itemsQuantity={7}
+        url="/products/hotdeals"
+      />
+      <MainCategoryTemplate
+        categoryName="Meals"
+        subCategories={[
+          "beverages",
+          "bakery",
+          "international",
+          "native",
+          "instantandsnacks",
+          "frozenfoods",
+          "veganandvegetarian",
+          "mealprep",
+        ]}
+      />
+      <MainSubCategories
+        categoryName="Meals"
+        subCategoryData={{
+          International: [
+            "Pizza",
+            "Shawarma",
+            "Burger",
+            "Pasta, Noodles & Spaghetti",
+          ],
+          Bakery: ["Breads", "Pastries", "Cakes", "Cookies"],
+          Instant: [
+            "Salads",
+            "Sandwiches & Wraps",
+            "Soups & Stews",
+            "Meal Kits",
+          ],
+          Beverages: [
+            "Coffee & Tea",
+            "Juices & Smoothies",
+            "Grains & Porridge",
+            "Ice Cream",
+          ],
+        }}
+      />
+      <MainItems
+        itemsName="Popular Meals"
+        itemsQuantity={7}
+        category="meal"
+        url="/meals/popular"
+      />
       <MainBrands />
-      <MainItems itemsName="Hot Deals" itemsQuantity={7} category="meal" />
-      <MainCategoryTemplate categoryName="Services" />
-      <MainSubCategories categoryName="Services" />
+      <MainItems
+        itemsName="Hot Deals"
+        itemsQuantity={7}
+        category="meal"
+        url="/meals/hotdeals"
+      />
+      <MainCategoryTemplate
+        categoryName="Services"
+        subCategories={[
+          "beautyandwellness",
+          "homeservices",
+          "tech",
+          "academic",
+          "logistics",
+          "events",
+        ]}
+      />
+      <MainSubCategories
+        categoryName="Services"
+        subCategoryData={{
+          "Beauty & Wellness": [
+            "Hairdressing",
+            "Manicures & Pedicures",
+            "Haircuts & Styling",
+            "Massage",
+          ],
+          Tech: [
+            "Web/App Development",
+            "Graphic Design",
+            "IT support",
+            "Repairs",
+          ],
+          Home: ["Cleaning", "Electrical", "Carpentry", "Plumbing"],
+          Educational: ["Tutoring", "Online courses", "Programs", "Events"],
+        }}
+      />
       <MainItems
         itemsName="Popular Services"
         itemsQuantity={7}
         category="service"
+        url="/services/popular"
       />
       <MainBrands />
-      <MainItems itemsName="Hot Deals" itemsQuantity={7} category="service" />
-      <MainCategoryTemplate categoryName="Stores" />
-      <MainSubCategories categoryName="Stores" />
-      <MainItems itemsName="Popular Stores" itemsQuantity={7} />
+      <MainItems
+        itemsName="Hot Deals"
+        itemsQuantity={7}
+        category="service"
+        url="/services/hotdeals"
+      />
+      <MainCategoryTemplate
+        categoryName="Stores"
+        subCategories={["Official"]}
+      />
+      <MainSubCategories
+        categoryName="Stores"
+        subCategoryData={{
+          Official: [
+            "Cell Phones",
+            "Phone Accessories",
+            "Storage & Memory",
+            "T-Shirts",
+          ],
+        }}
+      />
+      <MainItems
+        itemsName="Popular Stores"
+        itemsQuantity={7}
+        url="/products/popular"
+      />
       <MainBrands />
-      <MainItems itemsName="Hot Deals" itemsQuantity={7} />
+      <MainItems
+        itemsName="Hot Deals"
+        itemsQuantity={7}
+        url="/products/hotdeals"
+      />
     </section>
   );
 }
