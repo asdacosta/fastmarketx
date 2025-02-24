b3780b7c1ac75db1f359f58b03650212f959fa4a
asdacosta <asdacosta18@gmail.com>
2025-02-24 18:50:36 +0000

    Provide redux store to component

--- app/Main/Welcome/Welcome.tsx
+++ app/Main/Welcome/Welcome.tsx
@@ -1,15 +1,20 @@
+"use client";
 import React from "react";
 import Categories from "./Categories/Categories";
 import CardsBox from "./CardsBox/CardsBox";
 import Extras from "./Extras/Extras";
 import Promotion from "./Promotion/Promotion";
 import styles from "./Welcome.module.css";
+import { Provider } from "react-redux";
+import { store } from "@/app/redux/store";
 
 function Welcome() {
   return (
     <section className={styles.welcome}>
       <Categories />
-      <CardsBox />
+      <Provider store={store}>
+        <CardsBox />
+      </Provider>
       <section className={styles.extrasBox}>
         <Extras />
         <Promotion />
