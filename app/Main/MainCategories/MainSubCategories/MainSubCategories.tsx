b9cbc672885e45c1da5d7c47b803fb3ef97e9e3e
asdacosta <asdacosta18@gmail.com>
2025-02-24 18:51:14 +0000

    Refactor and define new props to render data

--- app/Main/MainCategories/MainSubCategories/MainSubCategories.tsx
+++ app/Main/MainCategories/MainSubCategories/MainSubCategories.tsx
@@ -5,9 +5,13 @@ import MainCategoryTemplate from "../MainCategoryTemplate/MainCategoryTemplate";
 
 interface MainSubCategories {
   categoryName: string;
+  subCategoryData: { [name: string]: string[] };
 }
 
-function MainSubCategories({ categoryName }: MainSubCategories) {
+function MainSubCategories({
+  categoryName,
+  subCategoryData,
+}: MainSubCategories) {
   const items = new Array(4).fill(1);
 
   const categoryClasses: { [key: string]: string } = {
@@ -19,64 +23,31 @@ function MainSubCategories({ categoryName }: MainSubCategories) {
 
   return (
     <section className={styles.mainSubCategories}>
-      {items.map((item, index) => {
-        return (
-          <section className={styles.subCategory} key={index}>
-            <section
-              className={`${styles.header} ${categoryClasses[categoryName]}`}
-            >
-              <span></span>
-              <h3>SubCategory</h3>
-            </section>
-            <section className={styles.items}>
-              <div className={styles.item}>
-                <Image
-                  quality={80}
-                  draggable="false"
-                  src="/mainImgs/products.avif"
-                  alt="Store"
-                  fill
-                  className={styles.img}
-                />
-                <span className={styles.cardName}>Item Name</span>
-              </div>
-              <div className={styles.item}>
-                <Image
-                  quality={80}
-                  draggable="false"
-                  src="/mainImgs/products.avif"
-                  alt="Store"
-                  fill
-                  className={styles.img}
-                />
-                <span className={styles.cardName}>Item Name</span>
-              </div>
-              <div className={styles.item}>
-                <Image
-                  quality={80}
-                  draggable="false"
-                  src="/mainImgs/products.avif"
-                  alt="Store"
-                  fill
-                  className={styles.img}
-                />
-                <span className={styles.cardName}>Item Name</span>
-              </div>
-              <div className={styles.item}>
-                <Image
-                  quality={80}
-                  draggable="false"
-                  src="/mainImgs/products.avif"
-                  alt="Store"
-                  fill
-                  className={styles.img}
-                />
-                <span className={styles.cardName}>Item Name</span>
-              </div>
-            </section>
+      {Object.entries(subCategoryData).map(([key, values]) => (
+        <section className={styles.subCategory} key={key}>
+          <section
+            className={`${styles.header} ${categoryClasses[categoryName]}`}
+          >
+            <span></span>
+            <h3>{key}</h3>
           </section>
-        );
-      })}
+          <section className={styles.items}>
+            {values.map((value, index) => (
+              <div className={styles.item} key={value}>
+                <Image
+                  quality={80}
+                  draggable="false"
+                  src="/mainImgs/products.avif"
+                  alt="Store"
+                  fill
+                  className={styles.img}
+                />
+                <span className={styles.cardName}>{value}</span>
+              </div>
+            ))}
+          </section>
+        </section>
+      ))}
     </section>
   );
 }
