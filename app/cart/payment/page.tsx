5edd3f8811a76782e95cb378aad6d4d6b4d6848b
asdacosta <asdacosta18@gmail.com>
2025-02-28 23:00:51 +0000

    Provide redux store

--- app/cart/payment/page.tsx
+++ app/cart/payment/page.tsx
@@ -5,6 +5,8 @@ import Header from "@/app/Header/Header";
 import LocationAddress from "./LocationAddress/LocationAddress";
 import CartSummary from "./CartSummary/CartSummary";
 import Payment from "./Payment/Payment";
+import { Provider } from "react-redux";
+import { store } from "../../redux/store";
 
 function Page() {
   const [receivingMethod, setReceivingMethod] = useState("payNow");
@@ -13,52 +15,54 @@ function Page() {
     <>
       <Header />
       <section className={styles.page}>
-        <section className={styles.paymentInfo}>
-          <LocationAddress />
-          <div className={styles.paymentMethod}>
-            <h2>SELECT PAYMENT METHOD (Door Delivery)</h2>
-            <div className={styles.methods}>
-              <label
-                className={`${styles.radioLabel} ${
-                  receivingMethod === "payNow" && styles.active
-                }`}
-              >
-                <input
-                  type="radio"
-                  value="payNow"
-                  checked={receivingMethod === "payNow"}
-                  onChange={(e) => setReceivingMethod(e.target.value)}
-                  className={styles.radioInput}
-                />
-                <span className={styles.customRadio}></span>
-                Pay now (Recommended)
-                <span className={styles.details}>
-                  Pay for delivery fee and items cost now
-                </span>
-              </label>
-              <label
-                className={`${styles.radioLabel} ${
-                  receivingMethod === "payOnDelivery" && styles.active
-                }`}
-              >
-                <input
-                  type="radio"
-                  value="payOnDelivery"
-                  checked={receivingMethod === "payOnDelivery"}
-                  onChange={(e) => setReceivingMethod(e.target.value)}
-                  className={styles.radioInput}
-                />
-                <span className={styles.customRadio}></span>
-                Pay on delivery
-                <span className={styles.details}>
-                  Pay for delivery fee now and pay for the items upon delivery
-                </span>
-              </label>
+        <Provider store={store}>
+          <section className={styles.paymentInfo}>
+            <LocationAddress />
+            <div className={styles.paymentMethod}>
+              <h2>SELECT PAYMENT METHOD (Door Delivery)</h2>
+              <div className={styles.methods}>
+                <label
+                  className={`${styles.radioLabel} ${
+                    receivingMethod === "payNow" && styles.active
+                  }`}
+                >
+                  <input
+                    type="radio"
+                    value="payNow"
+                    checked={receivingMethod === "payNow"}
+                    onChange={(e) => setReceivingMethod(e.target.value)}
+                    className={styles.radioInput}
+                  />
+                  <span className={styles.customRadio}></span>
+                  Pay now (Recommended)
+                  <span className={styles.details}>
+                    Pay for delivery fee and items cost now
+                  </span>
+                </label>
+                <label
+                  className={`${styles.radioLabel} ${
+                    receivingMethod === "payOnDelivery" && styles.active
+                  }`}
+                >
+                  <input
+                    type="radio"
+                    value="payOnDelivery"
+                    checked={receivingMethod === "payOnDelivery"}
+                    onChange={(e) => setReceivingMethod(e.target.value)}
+                    className={styles.radioInput}
+                  />
+                  <span className={styles.customRadio}></span>
+                  Pay on delivery
+                  <span className={styles.details}>
+                    Pay for delivery fee now and pay for the items upon delivery
+                  </span>
+                </label>
+              </div>
             </div>
-          </div>
-          <Payment />
-        </section>
-        <CartSummary />
+            <Payment />
+          </section>
+          <CartSummary />
+        </Provider>
       </section>
     </>
   );
