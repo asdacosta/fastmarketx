ec8970b672405cf1eeaea0cda61406d22647ff58
asdacosta <asdacosta18@gmail.com>
2025-02-28 22:58:47 +0000

    Provide redux store

--- app/Main/Main.tsx
+++ app/Main/Main.tsx
@@ -1,13 +1,18 @@
+"use client";
 import React from "react";
 import styles from "./Main.module.css";
 import Welcome from "./Welcome/Welcome";
 import MainCategories from "./MainCategories/MainCategories";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 
 function Main() {
   return (
     <main className={styles.main}>
       <Welcome />
-      <MainCategories />
+      <Provider store={store}>
+        <MainCategories />
+      </Provider>
     </main>
   );
 }
