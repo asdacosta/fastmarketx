28a020a921014c327720a0bdf7a0c5e24fe2dc5b
asdacosta <asdacosta18@gmail.com>
2025-02-28 22:38:42 +0000

    Get next custom data and use for Item component

--- app/Main/Welcome/Promotion/Promotion.tsx
+++ app/Main/Welcome/Promotion/Promotion.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import styles from "./Promotion.module.css";
 import Item from "../../MainCategories/Item/Item";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/redux/store";
+import { getNextAvailableItem } from "../../MainCategories/Item/getNextAvailableItem";
 
 function Promotion() {
+  const cartData = useSelector((state: RootState) => state.cart);
+  const itemData = getNextAvailableItem(cartData.items);
+
   return (
     <section className={styles.promotion}>
       <section>
         <h2>Today's Special Offer</h2>
       </section>
       <section className={styles.item}>
-        <Item />
+        <Item itemData={itemData} />
       </section>
     </section>
   );
