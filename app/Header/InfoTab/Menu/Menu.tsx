04d3194b6a976d8030d7c440d6081582a3a2bbc9
asdacosta <asdacosta18@gmail.com>
2025-03-18 00:31:55 +0000

    Add Menu component to display menu on click

---
+++ app/Header/InfoTab/Menu/Menu.tsx
@@ -0,0 +1,107 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./Menu.module.css";
+import Link from "next/link";
+import { DotLottieReact as Lot, DotLottie } from "@lottiefiles/dotlottie-react";
+import { useDispatch } from "react-redux";
+import { setMenu } from "@/app/redux/slices/MenuUiSlice";
+import CategoriesList from "./CategoriesList/CategoriesList";
+
+function Menu() {
+  const menuDispatch = useDispatch();
+  const [menuIcon, setMenuIcon] = useState<DotLottie | null>(null);
+  const [menuOpened, setMenuOpened] = useState(false);
+  const [categoriesOpen, setCategoriesOpen] = useState(false);
+
+  const toggleMenu = () => {
+    if (!menuIcon) return;
+
+    if (menuOpened) {
+      menuIcon.setMode("reverse");
+      setMenuOpened(false);
+    } else {
+      menuIcon.setMode("forward");
+      setMenuOpened(true);
+    }
+    menuIcon.setSpeed(7);
+    menuIcon.play();
+  };
+
+  return (
+    <section className={styles.menu}>
+      <Lot
+        className={styles.menuIcon}
+        onClick={toggleMenu}
+        dotLottieRefCallback={setMenuIcon}
+        src="https://raw.githubusercontent.com/asdacosta/assets/main/campusairx/menu.lottie"
+        style={{ width: "40px", height: "40px" }}
+      ></Lot>
+      <section
+        className={`${styles.menuList} ${menuOpened ? styles.show : ""}`}
+      >
+        <section className={styles.sign}>
+          <Link href="signup">Create Account</Link>
+          <Link href="login">Log In</Link>
+        </section>
+        <section className={styles.otherList}>
+          <section className={styles.categoriesBox}>
+            <div
+              className={styles.categories}
+              onClick={() => setCategoriesOpen((prev) => !prev)}
+            >
+              <svg viewBox="0 0 512 512">
+                <path d="M40 48C26.7 48 16 58.7 16 72l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24L40 48zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L192 64zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-288 0zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-288 0zM16 232l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24l-48 0c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24l-48 0z" />
+              </svg>
+              <span>Categories</span>
+              <svg
+                viewBox="0 0 320 512"
+                className={`${styles.chevron} ${
+                  categoriesOpen ? styles.open : ""
+                }`}
+              >
+                <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
+              </svg>
+            </div>
+            {categoriesOpen && (
+              <section className={styles.listBox}>
+                <CategoriesList />
+              </section>
+            )}
+          </section>
+          <Link href="/store">
+            <svg viewBox="0 0 576 512">
+              <path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
+            </svg>
+            <span>Store</span>
+          </Link>
+          <Link href="/menu" onClick={() => menuDispatch(setMenu("WishList"))}>
+            <svg role="img" viewBox="0 0 512 512">
+              <path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z" />
+            </svg>
+            <span>Wishlist</span>
+          </Link>
+          <Link href="/menu">
+            <svg role="img" viewBox="0 0 448 512">
+              <path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z" />
+            </svg>
+            <span>Menu</span>
+          </Link>
+          <Link href="/about">
+            <svg role="img" viewBox="0 0 192 512" className={styles.infoIcon}>
+              <path d="M48 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM0 224c0-17.7 14.3-32 32-32l64 0c17.7 0 32 14.3 32 32l0 224 32 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 512c-17.7 0-32-14.3-32-32s14.3-32 32-32l32 0 0-192-32 0c-17.7 0-32-14.3-32-32z" />
+            </svg>
+            <span>How it works</span>
+          </Link>
+          <Link href="logout">
+            <svg role="img" viewBox="0 0 512 512">
+              <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z" />
+            </svg>
+            <span>Log Out</span>
+          </Link>
+        </section>
+      </section>
+    </section>
+  );
+}
+
+export default Menu;
