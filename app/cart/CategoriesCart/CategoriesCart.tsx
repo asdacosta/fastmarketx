63448c0c79bb163362fac68c80707023775e6a0f
asdacosta <asdacosta18@gmail.com>
2025-03-26 18:28:42 +0000

    Remove unused import

--- app/cart/CategoriesCart/CategoriesCart.tsx
+++ app/cart/CategoriesCart/CategoriesCart.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./CategoriesCart.module.css";
 import Link from "next/link";
-import Image from "next/image";
 import CartItem from "./CartItem/CartItem";
 import { useSelector } from "react-redux";
 import { RootState } from "@/app/redux/store";
