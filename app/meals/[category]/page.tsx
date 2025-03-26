b131749cc693c5ea6864c06d790321219e891175
asdacosta <asdacosta18@gmail.com>
2025-03-26 18:33:48 +0000

    Capitalize to follow convention of component names

--- app/meals/[category]/page.tsx
+++ app/meals/[category]/page.tsx
@@ -146,7 +146,7 @@ const allowedItemsRoutes = ["speedy", "lowcost", "popular", "hotdeals", "new"];
 const itemsCount = new Array(27).fill(1);
 const itemsPerPage = 10;
 
-function page() {
+function Page() {
   const [currentPage, setCurrentPage] = useState(1);
   const params = useParams();
   const category = params?.category as string;
@@ -220,4 +220,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
