e8407c43c7da437db6785f732f51d045582cb7a1
asdacosta <asdacosta18@gmail.com>
2025-03-26 18:34:57 +0000

    Remove unused import

--- app/Main/MainCategories/Item/getNextAvailableItem.ts
+++ app/Main/MainCategories/Item/getNextAvailableItem.ts
@@ -1,14 +1,10 @@
-import { useSelector } from "react-redux";
 import { CartDataState } from "./ItemClient";
 import { itemsCustomData } from "./ItemsCustomData";
-import { RootState } from "@/app/redux/store";
 
 let nextAvailableItemIndex = 0;
 export function getNextAvailableItem(
   cartItems: CartDataState[]
 ): CartDataState {
-  const cartData = useSelector((state: RootState) => state.cart);
-
   const cartIds = cartItems.map((item) => item.id);
   for (let i = nextAvailableItemIndex; i < itemsCustomData.length; i++) {
     if (!cartIds.includes(itemsCustomData[i].id)) {
