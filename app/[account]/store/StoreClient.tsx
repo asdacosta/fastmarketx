a68873189582e6b93085296ad3dff985a96f2623
asdacosta <asdacosta18@gmail.com>
2025-03-26 18:28:21 +0000

    Clear unused import

--- app/[account]/store/StoreClient.tsx
+++ app/[account]/store/StoreClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import styles from "./page.module.css";
 import Dashboard from "./Dashboard/Dashboard";
 import Orders from "./Orders/Orders";
