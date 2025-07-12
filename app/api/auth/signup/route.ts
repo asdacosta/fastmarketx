873cf561c29b18f3597e8dcd056e060251af0777
asdacosta <asdacosta18@gmail.com>
2025-07-12 22:38:36 +0000

    Add route for user registration with JWT token

---
+++ app/api/auth/signup/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { connectDB } from "@/lib/db";
+import { User } from "@/models/User";
+import { signToken } from "@/lib/jwt";
+
+export async function POST(req: NextRequest) {
+  await connectDB();
+  const { name, gender, email, campus, password } = await req.json();
+
+  // If empty body, throw error
+  if (!name || !gender || !email || !password) {
+    return NextResponse.json({ error: "Complete all fields" }, { status: 400 });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user)
+    return NextResponse.json(
+      { error: "Email already in use" },
+      { status: 400 }
+    );
+
+  const passwordHash = await bcrypt.hash(password, 10);
+  const newUser = await User.create({
+    name,
+    gender,
+    email,
+    campus,
+    passwordHash,
+  });
+
+  const token = signToken(user._id.toString());
+  return NextResponse.json({
+    token,
+    user: { id: newUser._id, email: newUser.email },
+  });
+}
